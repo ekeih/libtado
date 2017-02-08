1b95ece9ba8d3b3a6717e1ba0d738525ec5a37a0
Max Rosin <git@hackrid.de>
2017-02-08 23:32:45 +0100

    Added a file header

--- tado/api.py
+++ tado/api.py
@@ -1,3 +1,37 @@
+# -*- coding: utf-8 -*-
+
+"""tado
+
+This module provides bindings to the API of https://www.tado.com/ to control
+your smart thermostats.
+
+Example:
+  import tado.api
+  t = tado.api('Username', 'Password')
+  print(t.get_me())
+
+Disclaimer:
+  This module is in NO way connected to tado GmbH and is not officially
+  supported by them!
+
+License:
+  Copyright (C) 2017  Max Rosin
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
 import json
 import requests
 
