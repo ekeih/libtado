f83fa21a5b2ac5a9441c65894b150f0d9a4d4251
Max Rosin <git@hackrid.de>
2017-02-10 03:15:01 +0100

    continued the POC of a command line client

--- libtado/__main__.py
+++ libtado/__main__.py
@@ -20,26 +20,22 @@ def main(ctx, username, password):
   """
   ctx.obj = libtado.api.Tado(username, password)
 
-@main.command(help='Tell me who the Tado API thinks I am.')
-@click.pass_obj
-def whoami(tado):
-  click.echo(tado.get_me())
 
-@main.command(help='Get all zones.')
+@main.command()
+@click.option('--zone', '-z', required=True, type=int, help='Zone ID')
 @click.pass_obj
-def zones(tado):
-  zones = tado.get_zones()
-  for zone in zones:
-    click.secho('%s (ID: %s)' % (zone['name'], zone['id']), fg='green', bg='black')
-    click.echo('Created: %s' % zone['dateCreated'])
-    click.echo('Type: %s' % zone['type'])
-    click.echo('Device Types: %s' % ', '.join(zone['deviceTypes']))
-    click.echo('Devices: %i' % len(zone['devices']))
-    click.echo('Dazzle: %s' % zone['dazzleEnabled'])
+def capabilities(tado, zone):
+  """Display the capabilities of a zone."""
+  click.echo(tado.get_capabilities(zone))
 
-@main.command(help='Get all devices.')
+
+@main.command(short_help='Display all devices.')
 @click.pass_obj
 def devices(tado):
+  """
+  Display all devices. If you have unsupported devices it will show you the
+  JSON output.
+  """
   devices = tado.get_devices()
   for d in devices:
     if d['deviceType'] == 'GW03':
@@ -61,16 +57,104 @@ def devices(tado):
       click.echo(d)
     click.echo('')
 
-@main.command(help='Set the temperature for a zone')
+
+@main.command(short_help='Display or change the early start feature of a zone.')
+@click.option('--zone', '-z', required=True, type=int, help='Zone ID')
+@click.option('--set', '-s', type=click.Choice(['on', 'off']))
+@click.pass_obj
+def early_start(tado, zone, set):
+  """Display the current early start configuration of a zone or change it."""
+  if set:
+    if set == 'on':
+      tado.set_early_start(zone, True)
+    elif set == 'off':
+      tado.set_early_start(zone, False)
+  else:
+    click.echo(tado.get_early_start(zone))
+
+
+@main.command()
+@click.pass_obj
+def home(tado):
+  """Display information about your home."""
+  click.echo(tado.get_home())
+
+
+@main.command()
+@click.pass_obj
+def mobile(tado):
+  """Display all mobile devices."""
+  click.echo(tado.get_mobile_devices())
+
+
+@main.command()
+@click.pass_obj
+def users(tado):
+  """Display all users of your home."""
+  click.echo(tado.get_users())
+
+
+@main.command(short_help='Tell me who the Tado API thinks I am.')
+@click.pass_obj
+def whoami(tado):
+  """
+  This command authenticates against the Tado API and asks for details about
+  the account you used to login. It is helpful to verify if your credentials
+  work.
+  """
+  me = tado.get_me()
+  click.echo('Name: %s' % me['name'])
+  click.echo('E-Mail: %s' % me['email'])
+  click.echo('Username: %s' % me['username'])
+  click.echo('Locale: %s' % me['locale'])
+  click.echo('Homes: %s' % me['homes'])
+  click.echo('Mobile Devices: %s' % me['mobileDevices'])
+
+
+@main.command(short_help='Get the current state of a zone.')
+@click.option('--zone', '-z', required=True, type=int, help='Zone ID')
+@click.pass_obj
+def zone(tado, zone):
+  """
+  Get the current state of a zone. Including temperature, humidity and 
+  heating power.
+  """
+  zone = tado.get_state(zone)
+  click.echo('Desired Temperature : %s' % zone['setting']['temperature']['celsius'])
+  click.echo('Current Temperature: %s' % zone['sensorDataPoints']['insideTemperature']['celsius'])
+  click.echo('Current Humidity: %s%%' % zone['sensorDataPoints']['humidity']['percentage'])
+  click.echo('Heating Power : %s%%' % zone['activityDataPoints']['heatingPower']['percentage'])
+  click.echo('Mode : %s' % zone['tadoMode'])
+  click.echo('Link : %s' % zone['link']['state'])
+
+
+@main.command(short_help='Get configuration information about all zones.')
+@click.pass_obj
+def zones(tado):
+  """Get configuration information about all zones."""
+  zones = tado.get_zones()
+  for zone in zones:
+    click.secho('%s (ID: %s)' % (zone['name'], zone['id']), fg='green', bg='black')
+    click.echo('Created: %s' % zone['dateCreated'])
+    click.echo('Type: %s' % zone['type'])
+    click.echo('Device Types: %s' % ', '.join(zone['deviceTypes']))
+    click.echo('Devices: %i' % len(zone['devices']))
+    click.echo('Dazzle: %s' % zone['dazzleEnabled'])
+
+
+@main.command()
 @click.option('--zone', '-z', required=True, type=int, help='Zone ID')
 @click.option('--temperature', '-t', required=True, type=int, help='Temperature')
 @click.option('--termination', '-x', default='MANUAL', help='Termination settings')
 @click.pass_obj
 def set_temperature(tado, zone, temperature, termination):
+  """Set the desired temperature of a zone."""
   tado.set_temperature(zone, temperature, termination=termination)
 
-@main.command(help='End manual control.')
+
+@main.command()
 @click.option('--zone', '-z', required=True, type=int, help='Zone ID')
 @click.pass_obj
 def end_manual_control(tado, zone):
+  """End manual control of a zone."""
   tado.end_manual_control(zone)
