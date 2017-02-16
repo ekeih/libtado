534f1866d598a261b2317cb99e69fed1afad2f58
Max Rosin <git@hackrid.de>
2017-02-16 16:26:10 +0100

    updated setup.py to enable pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-  name='Tado',
-  version='0.1',
+  name='libtado',
+  version='2.0.0.dev1',
   author='Max Rosin',
+  author_email='libtado@hackrid.de',
+  description='A library (and a command line client) to control your Tado Smart Thermostat.',
+  url='https://github.com/ekeih/libtado',
+  license='GPLv3+',
   packages=find_packages(),
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Programming Language :: Python :: 3',
+  ],
   install_requires=[
-    'Click',
+    'click',
+    'requests'
   ],
   entry_points={
     'console_scripts': [
